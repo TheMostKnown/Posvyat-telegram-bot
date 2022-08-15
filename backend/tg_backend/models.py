8adc15a235b35e7533817b253451ec615126f8b6
LostImagin4tion <danilov6083@outlook.com>
2022-08-15 17:16:16 +0300

    fix: fixed foreign keys in models

--- backend/tg_backend/models.py
+++ backend/tg_backend/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Organizers(models.Model):
     id = models.IntegerField(primary_key=True)
-    level = models.ForeignKey('level', on_delete=models.PROTECT)
-    room = models.ForeignKey('room', on_delete=models.PROTECT)
+    level = models.ForeignKey('Level', on_delete=models.PROTECT)
+    room = models.ForeignKey('Room', on_delete=models.PROTECT)
     surname = models.CharField(max_length=20)
     name = models.CharField(max_length=20)
     patronymic = models.CharField(max_length=20)
@@ -21,12 +21,12 @@ class Room(models.Model):
 
 class Guest(models.Model):
     id = models.IntegerField(primary_key=True)
-    level = models.ForeignKey('level', on_delete=models.PROTECT)
+    level = models.ForeignKey('Level', on_delete=models.PROTECT)
     surname = models.CharField(max_length=20)
     name = models.CharField(max_length=20)
     patronymic = models.CharField(max_length=20)
     phone = models.CharField(max_length=12)
-    room = models.ForeignKey('room', on_delete=models.PROTECT)
+    room = models.ForeignKey('Room', on_delete=models.PROTECT)
     tg_tag = models.CharField(max_length=20, unique=True)
     vk_link = models.CharField(max_length=20)
     texts = models.TextField()
@@ -35,11 +35,11 @@ class Guest(models.Model):
 
 class OrganizerSchedule(models.Model):
     id = models.IntegerField(primary_key=True)
-    tg_tag = models.ForeignKey('organizers', on_delete=models.PROTECT, to_field='tg_tag', related_name='tg_tag1')
+    tg_tag = models.ForeignKey('Organizers', on_delete=models.PROTECT, to_field='tg_tag', related_name='tg_tag1')
     desc = models.CharField(max_length=20)
     start_time = models.TimeField(auto_now=False, auto_now_add=False)
     finish_time = models.TimeField(auto_now=False, auto_now_add=False)
-    changer = models.ForeignKey('organizers', on_delete=models.PROTECT, to_field='tg_tag', related_name='tg_tag2')
+    changer = models.ForeignKey('Organizers', on_delete=models.PROTECT, to_field='tg_tag', related_name='tg_tag2')
 
 
 class GuestSchedule(models.Model):
@@ -56,14 +56,14 @@ class Level(models.Model):
 
 class Broadcast(models.Model):
     id = models.IntegerField(primary_key=True)
-    level = models.ForeignKey('level', on_delete=models.PROTECT)
+    level = models.ForeignKey('Level', on_delete=models.PROTECT)
     title = models.CharField(max_length=30)
     text = models.TextField()
 
 
 class Issue(models.Model):
     id = models.IntegerField(primary_key=True)
-    tg_tag = models.ForeignKey('organizers', on_delete=models.PROTECT, to_field='tg_tag')
+    tg_tag = models.ForeignKey('Organizers', on_delete=models.PROTECT, to_field='tg_tag')
     desc = models.TextField()
     status = models.CharField(max_length=256)
 
@@ -76,5 +76,5 @@ class Script(models.Model):
 
 class Button(models.Model):
     id = models.IntegerField(primary_key=True)
-    title_from = models.ForeignKey('script', on_delete=models.CASCADE)
+    title_from = models.ForeignKey('Script', on_delete=models.CASCADE)
     text = models.TextField()
