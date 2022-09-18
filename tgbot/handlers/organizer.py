8bbde164863dd97c5ec11efee53b6eda7c776c11
Геворг Цатурян <gstsaturyan@miem.hse.ru>
2022-09-18 17:33:57 +0300

    init

--- tgbot/handlers/organizer.py
+++ tgbot/handlers/organizer.py
@@ -1,6 +1,6 @@
 
 from tgbot.handlers import static_text as st
-
+from tgbot.models import Organizer
 
 def organizer(update, context):
     """ Show help info about all secret admins commands """
@@ -9,3 +9,10 @@ def organizer(update, context):
     #     return
 
     return update.message.reply_text(f'{st.secret_level}\n{st.secret_organizer_commands}')
+
+def room_info(update, context):
+    username = update.message.from_user['username']
+    try:
+        user = Organizer.get(tg_tag=username)
+    except Organizer.DoesNotExist:
+        return
