bf64d94bd6556938562eaf32e954d36aab742f89
aazadorozhnikova1 <aazadorozhnikova@miem.hse.ru>
2022-09-27 02:53:54 +0300

    fix

--- autoparser.py
+++ autoparser.py
@@ -1,6 +1,5 @@
 import django
 import os
-import logging
 import schedule
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dtb.settings')
@@ -10,26 +9,24 @@ from tgbot.handlers.spreadsheet_parser.commands.export_to_db import get_init_dat
 from dtb.settings import GOOGLE_TABLE_ID, GOOGLE_TOKEN_PATH, GOOGLE_CREDS_PATH
 
 
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
-)
-logger = logging.getLogger(__name__)
-logger.info('Initialize autoparser')
+
+print('Initialize autoparser')
+
+parsing_time = 2
 
 
 def autoparsing():
-    logger.info(GOOGLE_CREDS_PATH)
-    logger.info('AUTOPARSING!')
+    print(GOOGLE_CREDS_PATH)
+    print('AUTOPARSING!')
     get_init_data(
         spreadsheet_id=GOOGLE_TABLE_ID,
         creds_file_name=GOOGLE_CREDS_PATH,
         token_file_name=GOOGLE_TOKEN_PATH
     )
-    logger.info(f' Successfull AutoParsing ')
+    print(f' Successfull AutoParsing ')
 
 
-schedule.every(1).minutes.do(autoparsing)
+schedule.every(parsing_time).minutes.do(autoparsing)
 
 while True:
     schedule.run_pending()
